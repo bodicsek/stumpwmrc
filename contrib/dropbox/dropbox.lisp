7b6fdbcc043d81d658e2ff199d10890632f843bc
david.nabraczky <david.nabraczky@gmail.com>
2014-04-15 16:22:28 +0200

    fixing contrib directory structure and stumpwm function calls inside the modules

--- contrib/dropbox/dropbox.lisp
+++ contrib/dropbox/dropbox.lisp
@@ -7,7 +7,7 @@
 (defun fmt-dropbox (ml)
   (declare (ignore ml))
   (string-trim '(#\Space #\Tab #\Newline)
-	       (run-prog-collect-output "/usr/bin/dropbox" "status")))
+	       (stumpwm::run-prog-collect-output "/usr/bin/dropbox" "status")))
 
-(add-screen-mode-line-formatter #\D #'fmt-dropbox)
+(stumpwm::add-screen-mode-line-formatter #\D #'fmt-dropbox)
 
--- contrib/pulseaudio/pulseaudio.lisp
+++ contrib/pulseaudio/pulseaudio.lisp
@@ -6,15 +6,15 @@
 
 (defun volume-get-mute ()
   (string-trim '(#\Space #\Tab #\Newline)
-	       (run-shell-command
+	       (stumpwm::run-shell-command
 		"pactl list sinks | grep 'Mute:'| sed -r 's/.+Mute: (yes|no)/\\1/'"
 		t)))
 
 (defcommand volume-mute () ()
   "toggle volume mute state"
   (if (string= "yes" (volume-get-mute))
-      (run-shell-command "pactl set-sink-mute 0 0")
-      (run-shell-command "pactl set-sink-mute 0 1")))
+      (stumpwm::run-shell-command "pactl set-sink-mute 0 0")
+      (stumpwm::run-shell-command "pactl set-sink-mute 0 1")))
 
 (defcommand volume-raise () ()
   "raise the volume with 5%"
@@ -28,13 +28,13 @@
 
 (defun volume-get-percent ()
   (string-trim '(#\Space #\Tab #\Newline) 
-	       (run-shell-command 
+	       (stumpwm::run-shell-command 
 		"pactl list sinks | grep 'Volume: 0:'| sed -r 's/.+Volume: 0:\\s*([0-9]+)%.+/\\1/'" 
 		t)))
 
 (defun volume-run-command (modifier)
   (let ((next-vol-value (max 0 (round (/ (* #x10000 (funcall modifier (parse-integer (volume-get-percent)) 5)) 100)))))
-    (run-shell-command
+    (stumpwm::run-shell-command
      (concat "pactl set-sink-volume 0 0x" (write-to-string next-vol-value :base 16)))))
 
 
@@ -46,5 +46,5 @@
 	"---"
 	percent)))
 	
-(add-screen-mode-line-formatter #\V #'fmt-volume)
+(stumpwm::add-screen-mode-line-formatter #\V #'fmt-volume)
 
